93689c1ab2e87fdace821a4aa5085be2cdc40f7c
Pavan048 <pavankrishna048@gmail.com>
2024-05-18 11:51:14 +0530

    Frontend setup od the application

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+function App() {
+  return(
+    <div>
+    
+      Hello World
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/TourDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TourDetails = () => {
+  return (
+    <div>
+      TD
+    </div>
+  )
+}
+
+export default TourDetails
